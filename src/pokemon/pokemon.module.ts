184337a3886e57ba12c9263a1a62e324caa48b00
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2024-10-12 03:03:38 -0300

    se agrega schema de Pokemon

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './schemas/pokemon.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { 
+        name: Pokemon.name, 
+        schema: PokemonSchema 
+      }
+    ])
+  ],
+
   controllers: [PokemonController],
   providers: [PokemonService],
 })
