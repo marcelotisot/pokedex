a5bef536b37555959f8097bc1860c7b64c033bd1
Juan Marcelo Tisot <marcelotisot1990@gmail.com>
2024-10-12 02:49:11 -0300

    se configura la conexion con MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ 
       rootPath: join(__dirname,'..','public'), 
     }),
+
+    // Conexion con Mongo
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule 
   ],
 })
